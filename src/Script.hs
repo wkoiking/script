10adc75aa2a7b4bcea4b8dafc12f767c82a4827a
Nao Fujinaga <wanagida@gmail.com>
2022-06-24 03:55:17 +0530

    Fix getOnlyReachables
    Separated removeKnownHostAll and sshCopyIdAll

--- src/Script.hs
+++ src/Script.hs
@@ -20,11 +20,15 @@ srcFileDir = "/mnt/c/Users/wanag/Desktop/bin/2022-06-17-ew-ahmedabad"
 
 -- スクリプト
 
+removeKnownHostAll :: IO ()
+removeKnownHostAll = sh $ do
+    targetHost <- getOnlyReachables $ allServers ++ allWorkstations
+    removeKnownHost targetHost
+
 sshCopyIdAll :: IO ()
 sshCopyIdAll = sh $ do
-        targetHost <- getOnlyReachables $ allServers ++ allWorkstations
-        removeKnownHost targetHost
-        sshCopyID targetHost
+    targetHost <- getOnlyReachables $ allServers ++ allWorkstations
+    sshCopyID targetHost
 
 stopAllServersAndWorkstations :: IO ()
 stopAllServersAndWorkstations = do
