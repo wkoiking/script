b6a1ecf5c4502e2c3b5170c87db8dc43d40749d1
Nao Fujinaga <wanagida@gmail.com>
2022-06-25 02:42:50 +0530

    Fix password for ns-ahmedabad

--- src/Project.hs
+++ src/Project.hs
@@ -6,7 +6,7 @@ import Turtle
 user :: Text
 user = "mega"
 password :: Text
-password = "mega2021"
+password = "mega2018"
 workstationBinaryName :: FilePath
 workstationBinaryName = "hascats-exe-ws-ns"
 serverBinaryName :: FilePath
